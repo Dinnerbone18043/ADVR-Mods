da1b653bd1e19b1ef44c537ceb2d46448c8a80c1
Voided Child <ninjaking172@outlook.com>
2025-02-06 07:07:55 -0600

    added nightmare

---
+++ NIGHTMARE/nightmare.lua
@@ -0,0 +1,24 @@
+function onLoad()
+    base.loadAssetBundle("mods/NIGHTMARE/flashlight")
+end
+
+function onPlayerHit(damage)
+    return damage * 2
+end
+
+function onDungeonGenerated(worldGenerator)
+    debugLog("dungeon generated!")
+    if helperMethods.isInFirstDungeon(worldGenerator) == true then
+        debugLog("in first dungeon")
+        game.SetAmbientLight(col(0, 0, 0, 1))
+        debugLog("color set")
+        local flashlightTemplate = base.GetObjectFromAssetBundle("flashlight","UnityEngine.GameObject")
+        debugLog("template loaded")
+	    local flashlight = game.SpawnObjectLocal(flashlightTemplate, vector3.__new(0, 0, 0))
+        debugLog("spawned...???")
+	    flashlight.transform.SetParent(game.playerController.leftHand.transform)
+        debugLog("parent set")
+        flashlight.transform.rotation = game.playerController.leftHand.transform.rotation
+        debugLog("rotation set")
+    end
+end
